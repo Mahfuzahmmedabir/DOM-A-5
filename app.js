d319e58240384aeec80da786faf4fd8387df4561
mahfuzahmmedabir <mahfuz.abir06@gmail.com>
2024-09-23 23:02:57 +0100

    done history page

--- app.js
+++ app.js
@@ -2,27 +2,28 @@
 document.getElementById('btn-Noakhali').addEventListener('click', function () {
   const cash = getCurrentNumber('cash-balance');
   const getInput = getDonationInputNumber('Noakhali-input');
-  const donationNumber = getCurrentNumber('get-balance');
+  const donationNumber1 = getCurrentNumber('get-balance');
   if (isNaN(getInput) || getInput <= 0 || getInput > cash) {
     alert('Opps something worng');
     return alert;
   }
   const decreaseNumber = cash - getInput;
-  const addedNumber = getInput + donationNumber;
+  const addedNumber = getInput + donationNumber1;
   document.getElementById('cash-balance').innerText = decreaseNumber;
   document.getElementById('get-balance').innerText = addedNumber;
+  
 });
 // feni part
 document.getElementById('feni-btn').addEventListener('click', function () {
   const cash = getCurrentNumber('cash-balance');
   const getInput = getDonationInputNumber('feni-input');
-  const donationNumber = getCurrentNumber('get-balance-feni');
+  const donationNumber2 = getCurrentNumber('get-balance-feni');
   if (isNaN(getInput) || getInput <= 0 || getInput > cash) {
     alert('Oops something worng');
     return alert;
   }
   const decreaseNumber = cash - getInput;
-  const addedNumber = getInput + donationNumber;
+  const addedNumber = getInput + donationNumber2;
   document.getElementById('cash-balance').innerText = decreaseNumber;
   document.getElementById('get-balance-feni').innerText = addedNumber;
 });
@@ -30,13 +31,13 @@ document.getElementById('feni-btn').addEventListener('click', function () {
 document.getElementById('quota-btn').addEventListener('click', function () {
   const cash = getCurrentNumber('cash-balance');
   const getInput = getDonationInputNumber('quota-input');
-  const donationNumber = getCurrentNumber('quota-balance');
+  const donationNumber3 = getCurrentNumber('quota-balance');
   if (isNaN(getInput) || getInput <= 0 || getInput > cash) {
     alert('Oops something worng');
     return alert;
   }
   const decreaseNumber = cash - getInput;
-  const addedNumber = getInput + donationNumber;
+  const addedNumber = getInput + donationNumber3;
   document.getElementById('cash-balance').innerText = decreaseNumber;
   document.getElementById('quota-balance').innerText = addedNumber;
 });
@@ -46,11 +47,11 @@ document.getElementById('history-btn').addEventListener('click', function () {
   document.getElementById('history-btn').classList.add('bg-btn');
   document.getElementById('donate-histoy').classList.remove('hidden');
   document.getElementById('main-hide').classList.add('hidden');
-  // copy text
-  const donationNumber = getCurrentNumber('get-balance');
-  //  const donationNumber1 = getCurrentNumber('get-balance')
+  // dynamic history
+  const donationNumber1 = getCurrentNumber('get-balance');
+  const donationNumber2 = getCurrentNumber('get-balance-feni');
+  const donationNumber3 = getCurrentNumber('quota-balance');
 
-  // copy text end
   const div = document.createElement('div');
   console.log('hello');
   div.classList.add(
@@ -63,12 +64,46 @@ document.getElementById('history-btn').addEventListener('click', function () {
   );
   div.innerHTML = `
   <div class="p-4">
-  <h1 class ="text-left font-bold">software ${donationNumber}  </h1>
-  <h1 class ="text-left">Date: ${new Date()}(Bangladesh Time zone) </h1>
+  <h1 class ="text-left font-bold text-xl">${donationNumber1} Taka is Donated for famine-2024 at Feni, Bangladesh   </h1>
+  <p class ="text-left text-gray-600 mt-4">Date: ${new Date()}(Date:(Bangladesh Standard Time)  </p>
   </div>
   `;
-  console.log('heoo')
+  // 2
+
+  const div2 = document.createElement('div');
+  div2.classList.add(
+    'border-2',
+    'w-10/12',
+    'mx-auto',
+    'mt-10',
+    'h-32',
+    'rounded-xl'
+  );
+  div2.innerHTML = `
+  <div class="p-4">
+  <h1 class ="text-left font-bold text-xl"> ${donationNumber2} Taka is Donated for Flood Relief in Feni,Bangladesh   </h1>
+  <p class ="text-left text-gray-600 mt-4">Date: ${new Date()}(Date:(Bangladesh Standard Time)  </p>
+  </div>
+  `;
+  const div3 = document.createElement('div');
+
+  div3.classList.add(
+    'border-2',
+    'w-10/12',
+    'mx-auto',
+    'mt-10',
+    'h-32',
+    'rounded-xl'
+  );
+  div3.innerHTML = `
+  <div class="p-4">
+  <h1 class ="text-left font-bold text-xl">${donationNumber3} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh   </h1>
+  <p class ="text-left text-gray-600 mt-4 ">Date: ${new Date()}(Date:(Bangladesh Standard Time)</p>
+  </div>
+  `;
   document.getElementById('donate-histoy').append(div);
+  document.getElementById('donate-histoy').append(div2);
+  document.getElementById('donate-histoy').append(div3);
 });
 
 document.getElementById('donation').addEventListener('click', function () {
