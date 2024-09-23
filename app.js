19f573a13362e696082750ce1c3ae8e555d53ffa
mahfuzahmmedabir <mahfuz.abir06@gmail.com>
2024-09-23 12:45:41 +0100

    added postion sticky

--- app.js
+++ app.js
@@ -1,18 +1,52 @@
-// 1
 
+// Noakhali part
 document.getElementById('btn-Noakhali').addEventListener('click', function () {
-  const current = getCurrentNumber('current-balance');
+  const cash = getCurrentNumber('cash-balance');
   const getInput = getDonationInputNumber('Noakhali-input');
   const donationNumber = getCurrentNumber('get-balance');
-  if (isNaN(getInput) || getInput <= 0 || getInput > current ) {
+  if (isNaN(getInput) || getInput <= 0 || getInput > cash ) {
     alert("Opps something worng")
     return alert
   } 
-  const updetNumber = current - getInput;
-  const addeNumber = getInput + donationNumber;
-  document.getElementById('current-balance').innerText = updetNumber;
-  document.getElementById('get-balance').innerText = addeNumber;
+  const decreaseNumber = cash - getInput;
+  const addedNumber = getInput + donationNumber;
+  document.getElementById('cash-balance').innerText = decreaseNumber;
+  document.getElementById('get-balance').innerText = addedNumber;
+}); 
+// feni part 
+document.getElementById('feni-btn').addEventListener('click', function () {
+  const cash = getCurrentNumber('cash-balance');
+  const getInput = getDonationInputNumber('feni-input');
+  const donationNumber = getCurrentNumber('get-balance-feni');
+  if (isNaN(getInput) || getInput <= 0 || getInput > cash) {
+    alert('Oops something worng');
+    return alert;
+  }
+  const decreaseNumber = cash - getInput;
+  const addedNumber = getInput + donationNumber;
+  document.getElementById('cash-balance').innerText = decreaseNumber;
+  document.getElementById('get-balance-feni').innerText = addedNumber;
+});
+// quota part
+document.getElementById('quota-btn').addEventListener('click', function () {
+  const cash = getCurrentNumber('cash-balance');
+  const getInput = getDonationInputNumber('quota-input');
+  const donationNumber = getCurrentNumber('quota-balance');
+  if (isNaN(getInput) || getInput <= 0 || getInput > cash) {
+    alert('Oops something worng');
+    return alert;
+  }
+  const decreaseNumber = cash - getInput;
+  const addedNumber = getInput + donationNumber;
+  document.getElementById('cash-balance').innerText = decreaseNumber;
+  document.getElementById('quota-balance').innerText = addedNumber;
 });
 
+
+// history
+
+document.getElementById('history-btn').addEventListener('click', function () {
+   console.log("hek")
+});
 
 
